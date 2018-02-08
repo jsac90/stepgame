933cf877ca670d3b4f731881dc1289dc10d847c2
jsac90 <jsac90@gmail.com>
2018-02-08 04:22:16 -0800

    added rewards: Weapons, Armor, and HP Potions!

--- test.php
+++ test.php
@@ -6,14 +6,20 @@ $og_steps = 10000; //test only. Eventually pull this from fitbit.
 $steps = $og_steps; //set initial steps
 //$steprange = $steps / 5; //removed this 2/7 in favor of the 1 in 50 encounter rate. This might change though. 
 $level = 1; //test. this will be pulled from a database later. 
+$max_hp = (10 + (10*($level * 1.5)));
 $hp = (10 + (10*($level * 1.5)));
 $next_level = $level + 1;
-$player_attack = $level + 1; //test only. will eventually depend on gear
-$player_defense = $level + 1; //test only. will eventually depend on gear
+$has_weapon = 0;
+$has_armor = 0; 
+$weapon_power = 1; //fists by default. Power = 1
+$armor_power = 1; //clothes by default. Power = 1
+$player_attack = $level + $weapon_power ; //depends on gear
+$player_defense = $level + $armor_power; //depends on gear
 //$player_exp = 0; //test only
 $player_exp = (((37.5*(($level)**2))+(87.5*($level)))-124);
 $next_level_exp = (((37.5*(($next_level)**2))+(87.5*($next_level)))-124);
 $remaining_exp = $next_level_exp - $player_exp;
+
 
 ?>
 
@@ -29,8 +35,19 @@ echo "You are level $level <br>";
 echo "You have $player_exp total exp. In $remaining_exp exp you will reach level $next_level <br><br>";
 echo "You have $hp hit points <br>";
 echo "You have $player_attack attack and $player_defense defense <br><br>";
+if($has_weapon == 1){
+	echo "You have a weapon. It has $weapon_power power <br>";
+} else{
+	echo "You do not have a weapon. Just your fists! <br>";
+}
+if($has_armor == 1){
+	echo "You have some armor. It has $armor_power power <br>";
+} else {
+	echo "You do not have any armor. Just your clothes! <br>";
+}
 
-echo "-----------------<br><br>";
+
+echo "<br>-----------------<br><br>";
 
 while ($steps > 0 && $hp > 0) {
 	
@@ -57,6 +74,7 @@ while ($steps > 0 && $hp > 0) {
 			You have $steps steps remaining. <br>
 			Monster has $monster_hp hit points. <br>
 			Monster has $monster_attack attack and $monster_defense defense <br>
+			You have $player_attack attack and $player_defense defense <br>
 		";
 		
 		//determine who goes first 
@@ -98,23 +116,56 @@ while ($steps > 0 && $hp > 0) {
 		}
 		//win and lose statements.
 		if ($hp <= 0){
-			echo "Monster killed you!<br><br>";
+			echo "<font color=red><b>Monster knocked you out!</b></font> <br><br>";
 		} else if ($monster_hp <= 0){
-			echo "You killed monster<br>";
+			echo "You killed the monster<br>";
 			$gained_exp = $monster_level * (($monster_attack / $monster_level) + ($monster_defense / $monster_level)); 
 			echo "You gained $gained_exp exp points! <br>";
 			$player_exp = $player_exp + $gained_exp;
 			//this is where you need the if statement for levelups
 			$remaining_exp = $next_level_exp - $player_exp;
-			echo "$remaining_exp exp remaining until level $next_level <br><br>";
+			echo "$remaining_exp exp remaining until level $next_level <br>";
+			echo "You have $hp hit points remaining<br><br>";
 			
-			
+			//reward calculation
+			$reward_chance = rand(1,100);
+			if($reward_chance >= 80){ //20% chance to get something good
+				echo "You get a reward!<br>";
+				//pick the actual reward. if gear, will ALWAYS be better than what you have.
+				$reward_select = rand (1,100);
+				if ($reward_select <= 10){ //10% weapon. Randomly generates power level.
+					echo "You found a new weapon!<br>";
+					$has_weapon = 1;
+					$weapon_power = $weapon_power + $level + rand($weapon_power, ($weapon_Power + 5));
+					echo "New Weapon has $weapon_power power.<br>";
+					$player_attack = $level + $weapon_power ; //recalculates attack for moving forward
+					//usually 50 or 75 for armor but changed this for testing
+				} else if ($reward_select >10 && $reward_select <= 20){ //10% armor. Randomly generates power.
+					echo "You found new armor!<br>";
+					$has_armor = 1;
+					$armor_power = $armor_power + $level + rand($armor_power, ($armor_Power + 5));
+					echo "New armor has $armor_power power.<br>";
+					$player_defense = $level + $weapon_power ; //recalculates defense moving forward
+				} else { //potion - randomly generates how much it restores
+					echo "You found a health potion!<br>";
+					$restore_percent = rand(1,35);
+					$restore_amt = ceil($max_hp * ($restore_percent/100));
+					echo "Potion will restore up to $restore_percent percent of your health = $restore_amt pts <br>";
+					$hp = $hp + $restore_amt;
+					
+					if ($hp > $max_hp){
+						$hp = $max_hp; //make sure cant have more than max of your hp. 
+					}
+					
+					echo "You now have $hp hit points!<br><br>";
+					
+				}
+			}
 			
 		} else {
 			echo "someone died but we're not sure who...<br>";
 			exit();
 		}
-		
 		
 		echo "-----------------<br><br>";
 
@@ -129,15 +180,27 @@ while ($steps > 0 && $hp > 0) {
 if ($steps <= 0){
 	echo "You're out of energy. Come back with more steps<br><br>";
 } elseif ($hp <= 0) {
+	$hp = 0; //sets to zero if negative - just looks nicer
 	echo "You're out of HP. Come back later when your HP has recharged<br><br>";
 } else {
 	echo "You're being booted from the dungeon for some reason. This developer sucks lel<br><br>";
 }
 
-echo "You have $player_exp total experience <br>
-You will grow to level $next_level after gaining $remaining_exp to meet $next_level_exp exp <br><br>
-You have $steps steps remaining. <br><br>";
-
+echo "You end the day with $steps steps! <br>";
+echo "You are level $level <br>";
+echo "You have $player_exp total exp. In $remaining_exp exp you will reach level $next_level <br><br>";
+echo "You have $hp hit points <br>";
+echo "You have $player_attack attack and $player_defense defense <br><br>";
+if($has_weapon==1){
+	echo "You have a weapon. It has $weapon_power power <br>";
+} else{
+	echo "You do not have a weapon. Just your fists! <br>";
+}
+if($has_armor==1){
+	echo "You have some armor. It has $armor_power power <br>";
+} else {
+	echo "You do not have any armor. Just your clothes! <br>";
+}
 
 ?>
 <br><br>
